a55c50a04d142919f17cb894be86c2598724315d
S-Chanchon11 <chanchon.splus@gmail.com>
2023-04-30 13:31:26 +0700

    add linear search func.

--- app.java
+++ app.java
@@ -7,12 +7,14 @@ class userInput {
     String file;
     ArrayList<String> arr = new ArrayList<>();
     String sort[];
+    Boolean status;
     public userInput(String filename){
         File txtfile = new File(filename); 
         try {
             Scanner read = new Scanner(txtfile);
             while(read.hasNextLine()){
-                file = read.nextLine();        
+                file = read.nextLine();
+        
                 sort = file.split(",");                 
             }          
             read.close();
@@ -20,15 +22,22 @@ class userInput {
         for(String a : sort){ System.out.print(a + "\n"); } 
     }
     public void findUser(){
+        
+        System.out.print("ID:");   
         Scanner input = new Scanner(System.in);
-        if(input.equals(file)){           
+        String user = input.next();
+        for(int i=0;i<sort.length;i++){
+            status = user.contains(sort[i]);
+            if(status){break;}
         }
+        if(status){System.out.println("Found");}               
+        else System.out.println("Not Found"); 
     }
 }
 public class app {
     public static void main(String[] args) {
         System.out.println();
         userInput u = new userInput("info.txt");
-        //u.findUser();
+        u.findUser();
     }
 }
