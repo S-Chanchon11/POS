4687211529e42ddf71a1e84f35513072dceb5bd6
S-Chanchon11 <chanchon.splus@gmail.com>
2023-05-01 13:31:53 +0700

    add addUser() and deleteUser()

--- app.java
+++ app.java
@@ -1,43 +1,77 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
-class userInput {
+class userManagement {
     String file;
-    ArrayList<String> arr = new ArrayList<>();
+    int i,index;
     String sort[];
     Boolean status;
-    public userInput(String filename){
+    String filename = "info.txt";
+    Scanner input;
+    String user;
+    public userManagement(String filename){
         File txtfile = new File(filename); 
         try {
             Scanner read = new Scanner(txtfile);
             while(read.hasNextLine()){
                 file = read.nextLine();
-        
                 sort = file.split(",");                 
             }          
             read.close();
         } catch (FileNotFoundException e) { e.printStackTrace(); }
         for(String a : sort){ System.out.print(a + "\n"); } 
     }
-    public void findUser(){
-        
+    public int findUser(){
+        //find index of element
+        //if the function return -1 means it not found in the database
         System.out.print("ID:");   
-        Scanner input = new Scanner(System.in);
-        String user = input.next();
-        for(int i=0;i<sort.length;i++){
+        input = new Scanner(System.in);
+        user = input.next();
+        
+        for(i=0;i<sort.length;i++){
             status = user.contains(sort[i]);
+            
             if(status){break;}
         }
         if(status){System.out.println("Found");}               
         else System.out.println("Not Found"); 
+
+        return i;
+    }
+
+    public void addUser(){
+        System.out.print("ID:"); 
+        input = new Scanner(System.in);
+        user = input.next();
+        try{
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filename, true));
+            writer.append(",");
+            writer.append(user);
+            
+            input.close(); writer.close();
+        }catch(IOException e) { e.printStackTrace(); }   
+    }
+
+    public void deleteUser(){
+        int p;
+        //findUser();
+        p=findUser();
+        if(p>-1){
+            System.out.print(p);
+            
+        }
     }
 }
 public class app {
     public static void main(String[] args) {
         System.out.println();
-        userInput u = new userInput("info.txt");
-        u.findUser();
+        userManagement u = new userManagement("info.txt");
+        //u.findUser();
+        //u.addUser();
+        u.deleteUser();
     }
 }
